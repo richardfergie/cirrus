6ef515371e24affa26d928db32e05184d23d7da8
Richard Fergie <richard.fergie@gmail.com>
2015-09-17 01:36:23 +0100

    Proof that model file can be in different directory

--- web/Model.hs
+++ web/Model.hs
@@ -9,3 +9,6 @@ import Database.Persist.Quasi
 -- http://www.yesodweb.com/book/persistent/
 share [mkPersist sqlSettings, mkMigrate "migrateAll"]
     $(persistFileWith lowerCaseSettings "config/models")
+
+share [mkPersist sqlSettings, mkMigrate "migrateAll"]
+    $(persistFileWith lowerCaseSettings "../database/persistent-model")
