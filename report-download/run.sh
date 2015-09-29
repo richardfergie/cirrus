9998fb79f27751ed6c7ce30a519b5f61f968b57a
Richard Fergie <richard.fergie@gmail.com>
2015-09-29 15:04:19 +0100

    Uses shared volume to save reports

--- report-download/run.sh
+++ report-download/run.sh
@@ -8,7 +8,17 @@ if [ ! -z "$DATA_CONTAINER" ]
       echo "Data container is present"
   else
       echo "Creating data container"
-      docker create -v /opt/reports --name report-data busybox
+      docker create -v /opt/reports/ --name report-data busybox
 fi
 
-docker run --volumes-from report-data -e ADWORDS_CLIENT_ID=${ADWORDS_CLIENTID} -e ADWORDS_CLIENT_SECRET=${ADWORDS_CLIENT_SECRET} -e ADWORDS_DEVELOPER_TOKEN=${ADWORDS_DEVELOPER_TOKEN} -e ADWORDS_REFRESH_TOKEN=${ADWORDS_REFRESH_TOKEN} downloader python /download.py
+OLD_CONTAINER=$(docker ps -a | grep "adwords-download" | awk {'print $1'} | head -1)
+
+if [ ! -z "$OLD_CONTAINER" ]
+    then
+        docker stop $OLD_CONTAINER
+        docker rm $OLD_CONTAINER
+    else
+        echo "No old container"
+fi
+
+docker run --name adwords-download --volumes-from report-data -e ADWORDS_CLIENT_ID=${ADWORDS_CLIENTID} -e ADWORDS_CLIENT_SECRET=${ADWORDS_CLIENT_SECRET} -e ADWORDS_DEVELOPER_TOKEN=${ADWORDS_DEVELOPER_TOKEN} -e ADWORDS_REFRESH_TOKEN=${ADWORDS_REFRESH_TOKEN} report-downloader python /download.py
