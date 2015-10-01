4ccef3231e23b4c03da372daaa58611e5f84fcd5
Richard Fergie <richard.fergie@gmail.com>
2015-10-01 09:01:34 +0100

    Deploy script

---
+++ deploy-all.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -xe
+source secrets.sh
+
+tar -zcvf /tmp/database.tar.gz database/*
+scp /tmp/database.tar.gz $PRODUCTION_USER@$PRODUCTION_HOST:/tmp/
+
+ssh $PRODUCTION_USER@$PRODUCTION_HOST "tar xvzf /tmp/database.tar.gz && cd database && ./run.sh"
+
+cd migrate
+./build.sh
+
+tar -zcvf /tmp/dbmigrate.tar.gz migrate/*
+scp /tmp/dbmigrate.tar.gz $PRODUCTION_USER@$PRODUCTION_HOST:/tmp/
+
+ssh $PRODUCTION_USER@$PRODUCTION_HOST "tar xvzf /tmp/dbmigrate.tar.gz && cd migrate && ./run.sh"
+
+cd ..
+tar -zcvf /tmp/report-download.tar.gz report-download/*
+tar -zcvf /tmp/report-insert.tar.gz report-insert/*
+
+scp /tmp/report-download.tar.gz $PRODUCTION_USER@$PRODUCTION_HOST:/tmp/
+scp /tmp/report-insert.tar.gz $PRODUCTION_USER@$PRODUCTION_HOST:/tmp/
+
+ssh $PRODUCTION_USER@$PRODUCTION_HOST "tar xvzf /tmp/report-download.tar.gz && tar xvzf /tmp/report-insert.tar.gz"
+
+scp daily-cron.sh $PRODUCTION_USER@$PRODUCTION_HOST:~/
+
