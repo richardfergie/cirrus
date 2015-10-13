803ff05231270492edbd949d3a2799df3b071d19
Richard Fergie <richard.fergie@gmail.com>
2015-10-13 09:42:20 +0100

    Restore db from S3 stored dump

--- backup/db-backup.sh
+++ backup/db-backup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -xe
 
 ADWORDS_FILE=`date +"%Y-%m-%d"`.Adwords.sql
+COMPRESSED_FILE=${ADWORDS_FILE}.gz
 
 pg_dump -h "$POSTGRES_PORT_5432_TCP_ADDR" -p "$POSTGRES_PORT_5432_TCP_PORT" -U adwords -f /tmp/${ADWORDS_FILE}
 
-aws s3 cp /tmp/${ADWORDS_FILE} s3://${POSTGRES_BACKUP_S3_BUCKET}/${ADWORDS_FILE}
+gzip -c -9 /tmp/${ADWORDS_FILE} > /tmp/${COMPRESSED_FILE}
+
+aws s3 cp /tmp/${COMPRESSED_FILE} s3://${POSTGRES_BACKUP_S3_BUCKET}/${COMPRESSED_FILE} --region=us-east-1
---
+++ backup/restore-database.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -xe
+
+SQL_DUMP_FILE=$(aws s3 ls ${POSTGRES_BACKUP_S3_BUCKET} --region us-east-1 | tail -1 | awk '{print $4}')
+
+aws s3 cp s3://${POSTGRES_BACKUP_S3_BUCKET}/${SQL_DUMP_FILE} /tmp/adwords.sql.gz --region=us-east-1
+
+zcat /tmp/adwords.sql.gz | psql -h "$POSTGRES_PORT_5432_TCP_ADDR" -p "$POSTGRES_PORT_5432_TCP_PORT" -U adwords
