7578df291bd41ef9d96de70ac1233d819d9a1a3b
Richard Fergie <richard.fergie@gmail.com>
2015-10-13 09:43:32 +0100

    Wrap component deploys into functions.
    Eventually will (hopefully) lead to the ability to only deploy certain components

--- deploy-all.sh
+++ deploy-all.sh
@@ -1,29 +1,48 @@
 #!/bin/bash -xe
 source secrets.sh
 
-tar -zcvf /tmp/database.tar.gz database/*
-scp /tmp/database.tar.gz $PRODUCTION_USER@$PRODUCTION_HOST:/tmp/
+deploy_backup () {
+    tar -zcvf /tmp/backup.tar.gz backup/*
+    scp /tmp/backup.tar.gz $PRODUCTION_USER@$PRODUCTION_HOST:/tmp/
+    # backup docker image must be present for database deplot
+    ssh $PRODUCTION_USER@$PRODUCTION_HOST "tar xvzf /tmp/backup.tar.gz && cd backup && docker build --tag backup ."
+}
 
-ssh $PRODUCTION_USER@$PRODUCTION_HOST "tar xvzf /tmp/database.tar.gz && cd database && ./run.sh"
+deploy_database () {
+    tar -zcvf /tmp/database.tar.gz database/*
+    scp /tmp/database.tar.gz $PRODUCTION_USER@$PRODUCTION_HOST:/tmp/
+    # Start/restart database
+    ssh $PRODUCTION_USER@$PRODUCTION_HOST "tar xvzf /tmp/database.tar.gz && cd database && ./run.sh"
+}
 
-cd migrate
-./build.sh
+deploy_migrate () {
+    cd migrate
+    ./build.sh
+    tar -zcvf /tmp/dbmigrate.tar.gz migrate/*
+    scp /tmp/dbmigrate.tar.gz $PRODUCTION_USER@$PRODUCTION_HOST:/tmp/
+    ssh $PRODUCTION_USER@$PRODUCTION_HOST "tar xvzf /tmp/dbmigrate.tar.gz && cd migrate && ./run.sh"
+    cd ..
+}
 
-tar -zcvf /tmp/dbmigrate.tar.gz migrate/*
-scp /tmp/dbmigrate.tar.gz $PRODUCTION_USER@$PRODUCTION_HOST:/tmp/
+deploy_download () {
+    tar -zcvf /tmp/report-download.tar.gz report-download/*
+    scp /tmp/report-download.tar.gz $PRODUCTION_USER@$PRODUCTION_HOST:/tmp/
+    ssh $PRODUCTION_USER@$PRODUCTION_HOST "tar xvzf /tmp/report-download.tar.gz"
+}
 
-ssh $PRODUCTION_USER@$PRODUCTION_HOST "tar xvzf /tmp/dbmigrate.tar.gz && cd migrate && ./run.sh"
+deploy_insert () {
+    tar -zcvf /tmp/report-insert.tar.gz report-insert/*
+    scp /tmp/report-insert.tar.gz $PRODUCTION_USER@$PRODUCTION_HOST:/tmp/
+    ssh $PRODUCTION_USER@$PRODUCTION_HOST "tar xvzf /tmp/report-insert.tar.gz"
+}
 
-cd ..
-tar -zcvf /tmp/report-download.tar.gz report-download/*
-tar -zcvf /tmp/report-insert.tar.gz report-insert/*
-tar -zcvf /tmp/backup.tar.gz backup/*
+deploy_cron () {
+    scp daily-cron.sh $PRODUCTION_USER@$PRODUCTION_HOST:~/
+}
 
-scp /tmp/report-download.tar.gz $PRODUCTION_USER@$PRODUCTION_HOST:/tmp/
-scp /tmp/report-insert.tar.gz $PRODUCTION_USER@$PRODUCTION_HOST:/tmp/
-scp /tmp/backup.tar.gz $PRODUCTION_USER@$PRODUCTION_HOST:/tmp/
-
-ssh $PRODUCTION_USER@$PRODUCTION_HOST "tar xvzf /tmp/report-download.tar.gz && tar xvzf /tmp/report-insert.tar.gz && tar xvzf /tmp/backup.tar.gz"
-
-scp daily-cron.sh $PRODUCTION_USER@$PRODUCTION_HOST:~/
-
+deploy_backup
+deploy_database
+deploy_migrate
+deploy_download
+deploy_insert
+deploy_cron
