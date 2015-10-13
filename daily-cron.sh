2a4e88662fd4dc2090fa105c5f1515a6127b8ba2
Richard Fergie <richard.fergie@gmail.com>
2015-10-13 20:43:27 +0100

    Clean up old reports

--- daily-cron.sh
+++ daily-cron.sh
@@ -3,3 +3,5 @@
 source ~/secrets.sh
 cd ~/report-download && ./run.sh && cd ~/report-insert && ./run.sh
 cd ~/backup && ./run.sh
+# clean old reports
+docker run --volumes-from report-data "postgres:9.4" sh -c "find /opt/reports/* -mtime +7 -exec rm -v {} \;"
