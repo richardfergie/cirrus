bd1b9d61b064b042b548f286e7363ef459284676
Richard Fergie <richard.fergie@gmail.com>
2015-10-14 10:14:42 +0100

    Slightly better logging

--- daily-cron.sh
+++ daily-cron.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -xe
 
 source ~/secrets.sh
-cd ~/report-download && ./run.sh && cd ~/report-insert && ./run.sh
-cd ~/backup && ./run.sh
-# clean old reports
+echo "Running report download/insert"
+cd ~/report-download && ./run.sh && cd ~/report-insert && ./run.sh && echo "Download complete"
+echo "Running backup"
+cd ~/backup && ./run.sh && echo "Backup complete"
+echo "Cleaning old reports"
 docker run --volumes-from report-data "postgres:9.4" sh -c "find /opt/reports/* -mtime +7 -exec rm -v {} \;"
