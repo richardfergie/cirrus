40b623c587bec1e001ea947afd4e60269f7afe2d
Richard Fergie <richard.fergie@gmail.com>
2015-11-30 14:57:58 +0000

    Clean up docker containers for disk space

--- daily-cron.sh
+++ daily-cron.sh
@@ -7,3 +7,7 @@ echo "Running backup"
 cd ~/backup && ./run.sh && echo "Backup complete"
 echo "Cleaning old reports"
 docker run --volumes-from report-data "postgres:9.4" sh -c "find /opt/reports/* -mtime +7 -exec rm -v {} \;"
+
+echo "Cleaning Docker containers"
+docker rm $(docker ps -a -q)
+docker rmi $(docker images | grep "^<none>" | awk "{print $3}")
