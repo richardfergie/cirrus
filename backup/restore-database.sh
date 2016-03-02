9b034dd07a5153c61ab8259351f9cd7858074753
Richard Fergie <richard.fergie@gmail.com>
2016-03-02 17:02:24 +0000

    Switch database user to allow proper restore

--- backup/restore-database.sh
+++ backup/restore-database.sh
@@ -4,4 +4,4 @@ SQL_DUMP_FILE=$(aws s3 ls ${POSTGRES_BACKUP_S3_BUCKET} --region us-east-1 | tail
 
 aws s3 cp s3://${POSTGRES_BACKUP_S3_BUCKET}/${SQL_DUMP_FILE} /tmp/adwords.sql.gz --region=us-east-1
 
-zcat /tmp/adwords.sql.gz | psql -h "$POSTGRES_PORT_5432_TCP_ADDR" -p "$POSTGRES_PORT_5432_TCP_PORT" -U adwords
+zcat /tmp/adwords.sql.gz | psql -h "$POSTGRES_PORT_5432_TCP_ADDR" -p "$POSTGRES_PORT_5432_TCP_PORT" -U postgres
--- database/run.sh
+++ database/run.sh
@@ -104,7 +104,7 @@ EOF
 if [ "$tables" == "No relations found." ];
 then
     echo "No relations found - loading backup"
-    docker run --label "type=tmp" --link cirrus-postgres:postgres -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} -e POSTGRES_BACKUP_S3_BUCKET=${POSTGRES_BACKUP_S3_BUCKET} -e PGPASSWORD=${ADWORDS_PG_PASSWORD} backup /restore-database.sh
+    docker run --label "type=tmp" --link cirrus-postgres:postgres -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} -e POSTGRES_BACKUP_S3_BUCKET=${POSTGRES_BACKUP_S3_BUCKET} -e PGPASSWORD=${POSTGRES_PG_PASSWORD} backup /restore-database.sh
 else
     echo "Data already in place. Sweet"
 fi
