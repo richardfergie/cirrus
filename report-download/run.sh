71fbc88c70391791d87d6e564ed876ed091c59ef
Richard Fergie <richard.fergie@gmail.com>
2016-04-06 12:48:41 +0100

    Download and insert working with multi-account

--- report-download/run.sh
+++ report-download/run.sh
@@ -21,4 +21,17 @@ if [ ! -z "$OLD_CONTAINER" ]
         echo "No old container"
 fi
 
-docker run --label "type=tmp" --name adwords-download --volumes-from report-data -e ADWORDS_CLIENT_ID=${ADWORDS_CLIENT_ID} -e ADWORDS_CLIENT_SECRET=${ADWORDS_CLIENT_SECRET} -e ADWORDS_DEVELOPER_TOKEN=${ADWORDS_DEVELOPER_TOKEN} -e ADWORDS_REFRESH_TOKEN=${ADWORDS_REFRESH_TOKEN} report-downloader python /download.py
+IFS=$'\n'
+
+accounts=$(docker run -i --label type=tmp --link cirrus-postgres:postgres -e PGPASSWORD=${POSTGRES_PG_PASSWORD} --rm postgres:9.4 sh -c 'exec psql -t -h "$POSTGRES_PORT_5432_TCP_ADDR" -p "$POSTGRES_PORT_5432_TCP_PORT" -U postgres admin'<<-EOF
+SELECT DISTINCT client_id FROM adwords_account
+EOF
+        )
+
+for account in $accounts
+do
+    client_id=$(echo $account | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
+    echo "Downloading reports for ${client_id}"
+    docker run --label "type=tmp" --name adwords-download --volumes-from report-data -e ADWORDS_CLIENT_ID=${ADWORDS_CLIENT_ID} -e ADWORDS_CLIENT_SECRET=${ADWORDS_CLIENT_SECRET} -e ADWORDS_DEVELOPER_TOKEN=${ADWORDS_DEVELOPER_TOKEN} -e ADWORDS_REFRESH_TOKEN=${ADWORDS_REFRESH_TOKEN} -e CLIENT_ID=${client_id} report-downloader python /download.py
+    echo "Downloaded reports for ${client_id}"
+done
