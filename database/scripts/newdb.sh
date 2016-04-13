c422a802b0472b1c219e0c5e77cb0d00b92131b7
Richard Fergie <richard.fergie@gmail.com>
2016-04-13 13:22:16 +0100

    Scripts for database tasks

---
+++ database/scripts/newdb.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+
+DBNAME=$1
+DBUSER=$2
+DBPASS=$3
+
+createuser -h "$POSTGRES_PORT_5432_TCP_ADDR" -p "$POSTGRES_PORT_5432_TCP_PORT" -U postgres ${DBUSER}
+
+createdb -h "$POSTGRES_PORT_5432_TCP_ADDR" -p "$POSTGRES_PORT_5432_TCP_PORT" -U postgres ${DBNAME} --owner postgres
+
+psql -h "$POSTGRES_PORT_5432_TCP_ADDR" -p "$POSTGRES_PORT_5432_TCP_PORT" -U postgres ${DBNAME} <<-EOF
+ALTER USER ${DBUSER} WITH PASSWORD '${DBPASS}';
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO ${DBUSER};
+ALTER DEFAULT PRIVILEGES IN SCHEMA public
+   GRANT SELECT ON TABLES TO ${DBUSER};
+EOF
+
+cd /sql/adwords
+
+for f in *.sql
+do
+    echo "Running $f"
+    cat $f | psql -h "$POSTGRES_PORT_5432_TCP_ADDR" -p "$POSTGRES_PORT_5432_TCP_PORT" -U postgres ${DBNAME}
+done
