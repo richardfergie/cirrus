c3c1efe62902a9a88c9cb2e55c103d0bcdbfca82
Richard Fergie <richard.fergie@gmail.com>
2016-04-20 13:35:50 +0100

    Cleanup commented out code

--- web/build.sh
+++ web/build.sh
@@ -2,12 +2,6 @@
 echo $(git log --pretty=format:'%H' -n 1) > version
 echo $(date) >> version
 
-#sudo docker build --tag=potentiallabbuild .
-# next line is needed to get a containerid rather than an image
-#sudo docker run potentiallabbuild echo "Image built"
-#CONTAINER_ID=$(sudo docker ps -q -n=1) #won't work if another build happens in between
-#echo "Docker container " $CONTAINER_ID
-#sudo docker cp $CONTAINER_ID:/.cabal/bin/potential-lab deploy/
 whoami
 touch Settings/StaticFiles.hs
 stack setup
